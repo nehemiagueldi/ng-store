ef3872ec6164d487cdeb2d1b9608fa08d5073294
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2023-07-04 13:27:31 +0700

    Edit Image Link

--- pages/home.vue
+++ pages/home.vue
@@ -7,7 +7,7 @@ const selectedAccordion = ref("1");
     <!-- HERO -->
     <div class="hero min-h-screen bg-base-200 pt-20 md:pt-10">
       <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src="heroimg1.jpg" class="max-w-[18rem] md:max-w-sm rounded-lg shadow-2xl w-[450px] h-[600]" />
+        <img src="/heroimg1.jpg" class="max-w-[18rem] md:max-w-sm rounded-lg shadow-2xl w-[450px] h-[600]" />
         <div class="text-center lg:text-left">
           <h1 class="text-3xl lg:text-5xl font-bold">NG STORE</h1>
           <p class="py-6 md:pr-6">Explore NG Store, an innovative online marketplace offering livable houses, used goods, and more. With a wide range of products and affordable options, we cater to diverse needs and provide a delightful shopping experience. Our commitment lies in providing top-notch merchandise that is both functional and affordable. Whether you're searching for a cozy home or unique pre-owned items.</p>
@@ -20,7 +20,7 @@ const selectedAccordion = ref("1");
       <h1 class="text-center font-bold text-3xl pt-3 uppercase">Items</h1>
       <div class="flex justify-center items-center gap-4 pt-3 px-3 md:px-0 flex-wrap">
         <div class="card w-72 md:w-80 lg:w-96 bg-base-100 shadow-xl py-3">
-          <figure><img src="rpg1.jpeg" alt="Shoes" class="w-[325px] h-[325px] md:w-[385px] md:h-[385px]" /></figure>
+          <figure><img src="/rpg1.jpeg" alt="Shoes" class="w-[325px] h-[325px] md:w-[385px] md:h-[385px]" /></figure>
           <div class="card-body">
             <h2 class="card-title text-lg md:text-xl">Rumah Hook Daerah Jati Asih</h2>
             <p class="text-sm md:text-base">Di Jual Rumah Hook (Pojok) Strategis dengan Suasana Tenang dan Nyaman</p>
@@ -30,7 +30,7 @@ const selectedAccordion = ref("1");
           </div>
         </div>
         <div class="card w-72 md:w-80 lg:w-96 bg-base-100 shadow-xl py-3">
-          <figure><img src="rumahbandung1.png" class="w-[325px] h-[325px] md:w-[385px] md:h-[385px]" alt="Shoes" /></figure>
+          <figure><img src="/rumahbandung1.png" class="w-[325px] h-[325px] md:w-[385px] md:h-[385px]" alt="Shoes" /></figure>
           <div class="card-body">
             <h2 class="card-title text-lg md:text-xl">Rumah Daerah Kab.Bandung</h2>
             <p class="text-sm md:text-base">Di Jual Rumah Daerah Kab.Bandung Strategis Bebas Banjir</p>
@@ -40,7 +40,7 @@ const selectedAccordion = ref("1");
           </div>
         </div>
         <div class="card w-72 md:w-80 lg:w-96 bg-base-100 shadow-xl py-3">
-          <figure><img src="rumahbogor1.png" class="w-[325px] h-[325px] md:w-[385px] md:h-[385px]" alt="Shoes" /></figure>
+          <figure><img src="/rumahbogor1.png" class="w-[325px] h-[325px] md:w-[385px] md:h-[385px]" alt="Shoes" /></figure>
           <div class="card-body">
             <h2 class="card-title text-lg md:text-xl">Rumah Minimalis Daerah Bogor</h2>
             <p class="text-sm md:text-base">Di Jual Rumah Minimalis 2 Lantai Strategis dengan Suasana Tenang dan Nyaman</p>
@@ -92,7 +92,7 @@ const selectedAccordion = ref("1");
     <div class="px-3 md:px-20 lg:px-56 xl:px-96 mb-5">
       <h1 class="text-center font-bold text-3xl py-3 uppercase">Contact</h1>
       <div class="card card-side bg-base-100 shadow-xl">
-        <figure><img src="whatsapp.jpg" class="w-[250px] h-[350px]" alt="Movie" /></figure>
+        <figure><img src="/whatsapp.jpg" class="w-[250px] h-[350px]" alt="Movie" /></figure>
         <div class="card-body">
           <h2 class="card-title text-lg md:text-xl">Contact on Whatsapp</h2>
           <p class="text-sm md:text-base">Click the button for contact, feel free to ask 👍</p>
